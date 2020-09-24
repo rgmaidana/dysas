c8776797f0c9b57f4659fcdfb3df366f3fe89352
Renan Maidana <renanmaidana@gmail.com>
2020-09-24 16:12:14 +0200

    Updated package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='python-mss',
+setup(name='MarineSystemSim',
       version='0.1',
       description='Python package for Marine Systems Simulation (e.g., models for marine vessels)',
       url='http://www.github.com/rgmaidana/python-mss',
       author='Renan Maidana',
       author_email='renan.g.maidana@ntnu.no',
       license='MIT',
-      packages=['MSS'],
+      packages=['MarineSystemSim'],
       install_requires=[
           'numpy',
           'scipy'
